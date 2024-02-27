734eb19b1d4f5c7cff5af78638da1e9fafced44f
Lutz Karau <lutz.karau@ers-hameln.de>
2024-02-27 06:34:24 +0100

    Tutorialwelt #4 hinzugefügt

---
+++ App/Worlds/ClassesWorld04/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using KWEngine3;
+using KWEngine3.GameObjects;
+using KWEngine3.Helper;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace KWEngine3_Tutorial.App.Worlds.ClassesWorld04
+{
+    public class Player : GameObject
+    {
+        public override void Act()
+        {
+            HandleMovement();
+
+            List<Intersection> intersections = GetIntersections();
+            foreach (Intersection intersection in intersections)
+            {
+                if(intersection.Object is Enemy)
+                {
+                    MoveOffset(intersection.MTV);
+                }
+            }
+        }
+
+        private void HandleMovement()
+        {
+            if (Keyboard.IsKeyDown(Keys.A))
+            {
+                MoveOffset(-0.01f, 0f, 0f);
+            }
+            if (Keyboard.IsKeyDown(Keys.D))
+            {
+                MoveOffset(+0.01f, 0f, 0f);
+            }
+            if (Keyboard.IsKeyDown(Keys.W))
+            {
+                MoveOffset(0f, 0f, -0.01f);
+            }
+            if (Keyboard.IsKeyDown(Keys.S))
+            {
+                MoveOffset(0f, 0f, +0.01f);
+            }
+            if (Keyboard.IsKeyDown(Keys.Q))
+            {
+                MoveOffset(0f, -0.01f, 0f);
+            }
+            if (Keyboard.IsKeyDown(Keys.E))
+            {
+                MoveOffset(0f, +0.01f, 0f);
+            }
+        }
+    }
+}
