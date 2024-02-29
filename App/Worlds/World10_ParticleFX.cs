945edb216a551b68638737e10ca6626cc5e3fc52
Lutz Karau <lutz.karau@ers-hameln.de>
2024-02-29 06:48:59 +0100

    added world10

---
+++ App/Worlds/World10_ParticleFX.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using KWEngine3;
+using KWEngine3.GameObjects;
+using KWEngine3.Helper;
+using KWEngine3_Tutorial.App.Worlds.ClassesWorld10;
+using OpenTK.Graphics.ES11;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace KWEngine3_Tutorial.App.Worlds
+{
+    public class World10_ParticleFX : World
+    {
+        private Player _p;
+
+        public override void Act()
+        {
+            
+        }
+
+        public override void Prepare()
+        {
+            KWEngine.LoadModel("Robot", "./App/Models/robotERS.fbx");
+
+            SetCameraPosition(0f, 10f, 0f);
+            SetCameraTarget(0f, 0f, 0f);
+            SetColorAmbient(1f, 1f, 1f);
+            SetBackgroundFillColor(0.25f, 0.25f, 0.25f);
+
+            _p = new Player();
+            _p.SetModel("Robot");
+            _p.Name = "Player #1";
+            _p.SetPosition(0f, 0f, 0f);
+            _p.SetScale(1f);
+            AddGameObject(_p);
+        }
+    }
+}
